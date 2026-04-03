9f027706e9a566ce80edb8c0a109d692cc89d7ac
justsitesandappss <justsitesandapps@gmail.com>
2026-04-03 07:34:58 +0200

    s

--- app/nos-talents/riles-freestyle/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/nos-talents/riles-freestyle/page.tsx ou où tu veux
-import RilesFreestyle from "@/components/pages/talents/RilesFreestyle"
-
-export default function RilesPage() {
-  return <RilesFreestyle />
-}
